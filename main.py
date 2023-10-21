388af98ea49b4ad2a7c1dbb5fa1bee7b00131398
Alex H <alexjh11@gmail.com>
2023-10-21 12:32:48 -0700

    Updates

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from multiprocessing import Pool
 from py2neo import Graph, Node, Relationship
 import pandas as pd
 import time
+import pyarrow.csv as csv
+from io import StringIO
+import matplotlib.pyplot as plt
 
 graph = Graph("bolt://localhost:7687", auth=("neo4j", "password"))
 
@@ -24,29 +27,109 @@ def upload_edges(edges):
             tx.create(relationship)
     tx.commit()
 
+chunksize = 20
+batch_size = 20
+num_workers = 10
+
 if __name__ == '__main__':
+    
+#-------------------------------------Parallel Processing-------------------------------------
 
-    nodes = pd.read_csv('nodes.csv')
-    edges = pd.read_csv('edges.csv')
+    t0 = time.time()
+    nodes = pd.read_csv('nodes.csv', chunksize=chunksize)
 
-    batch_size = 50
-    num_workers = 10
-
-    t1 = time.time()
-    print('Uploading nodes')
-    node_batches = [nodes[i:i+batch_size] for i in range(0, len(nodes), batch_size)]
     with Pool(num_workers) as pool:
-        pool.map(upload_nodes, node_batches)
-    t2 = time.time() - t1
-    print(f'Nodes uploaded in {t2}')
+        t1 = time.time()
+        print('Uploading nodes')
+        #node_batches = [nodes[i:i+batch_size] for i in range(0, chunksize, batch_size)]
+        #with Pool(num_workers) as pool:
+        pool.map(upload_nodes, nodes)
+        #for batch in node_batches:
+        #   upload_nodes(batch)
+        t2 = time.time() - t1
+        print(f'Nodes uploaded in {t2} seconds')
 
-    t1 = time.time()
-    print('Uploading edges')
-    edge_batches = [edges[i:i+batch_size] for i in range(0, len(edges), batch_size)]
-    # with Pool(num_workers) as pool:
-    #     pool.map(upload_edges, edge_batches)
-    for batch in edge_batches:
-        upload_edges(batch)
-    t2 = time.time() - t1
-    print(f'Edges uploaded in {t2}')
-        
+    node_time = time.time() - t0
+    print(f'Processing completed in {node_time} seconds')
+
+    t0 = time.time()
+    edges = pd.read_csv('edges.csv', chunksize=chunksize)
+
+    with Pool(num_workers) as pool:
+
+        t1 = time.time()
+        print('Uploading edges')
+        #edge_batches = [edges[i:i+batch_size] for i in range(0, chunksize, batch_size)]
+        #with Pool(num_workers) as pool:
+        pool.map(upload_edges, edges)
+        #for batch in edge_batches:
+        #    upload_edges(batch)
+        t2 = time.time() - t1
+        print(f'Edges uploaded in {t2} seconds')
+
+    edge_time = time.time() - t1
+    print(f'Processing completed in {edge_time} seconds')
+
+## Benchmarking
+
+#---------------------------------Bulk Upload-------------------------------------------------------
+
+    from neo4j import GraphDatabase
+
+    driver = GraphDatabase.driver("bolt://localhost:7687", auth=("neo4j", "password"))
+
+    reset = '''
+    MATCH (n)
+    DETACH DELETE n
+    '''
+
+    node_query = '''
+    LOAD CSV WITH HEADERS FROM 'file:///nodes.csv' as row
+    CALL {
+        with row
+        MERGE (node:Node {id:toInteger(row.osmid), lat: toFloat(row.lat), lon: toFloat(row.lon)})
+    } IN TRANSACTIONS OF 1000 ROWS
+    '''
+
+    node_count_query = 'MATCH (n:Node) return Count(*) as count'
+
+    with driver.session(database='neo4j') as session:
+        session.run(reset)
+        t1 = time.time()
+        session.run(node_query)
+        bulk = time.time() - t1
+        print(f'Nodes uploaded in {bulk} seconds')
+        result = session.run(node_count_query)    
+        data = result.data()
+        cnt = data[0]['count']
+        print("Number of nodes: \n",cnt)
+        session.run(reset)
+
+#----------------------------------Regular upload-----------------------------------
+
+    t0 = time.time()
+    nodes = pd.read_csv('nodes.csv', chunksize=chunksize)
+    for chunk in nodes:
+        upload_nodes(chunk)
+    end = time.time() - t0
+    print(f'Nodes uploaded in {end} seconds')
+
+    print(str(node_time) + ' ' + str(bulk) + ' ' + str(end))
+
+    data = {
+        'Upload': ['Parallel', 'Bulk', 'Regular'],
+        'Time (seconds)': [node_time, bulk, end]
+    }
+
+    def addlabels(x,y):
+        for i in range(len(x)):
+            plt.text(i, y[i]//2, y[i], ha = 'center')
+
+    df = pd.DataFrame(data)
+    ax = df.plot(x='Upload', y='Time (seconds)', kind='bar', legend=True)
+    plt.xlabel('Upload Type')
+    plt.ylabel('Time (seconds)')
+    plt.title('Upload time for nodes (17,414 nodes)')
+    addlabels(df['Upload'], df['Time (seconds)'])
+    plt.savefig('upload_times.png', bbox_inches='tight')
+    plt.show()
